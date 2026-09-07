487544faf73e74099a6f1f710b00841c12a5957a
dong4j <dong4j@gmail.com>
2026-09-07 19:38:34 +0800

    fix(cask): 对齐 Sparkle 构建版本

--- Casks/starcat.rb
+++ Casks/starcat.rb
@@ -1,8 +1,8 @@
 cask "starcat" do
-  version "1.6.0"
+  version "1.6.0,202609071918"
   sha256 "2257190ca44f7d4e3fdc01fd8b2a22b49ca22cd02426247410c52a847eefd513"
 
-  url "https://starcat.ink/downloads/Starcat-#{version}-arm64.dmg"
+  url "https://starcat.ink/downloads/Starcat-#{version.csv.first}-arm64.dmg"
   name "Starcat"
   desc "GitHub Stars manager and AI knowledge organizer"
   homepage "https://starcat.ink/"
