fed5c8aeaece8e7af0eb4fdde47e12ee60cafa4d
dong4j <dong4j@gmail.com>
2026-09-16 00:49:19 +0800

    chore(cask): 发布 Starcat 1.7.0

--- Casks/starcat.rb
+++ Casks/starcat.rb
@@ -1,6 +1,6 @@
 cask "starcat" do
-  version "1.6.1"
-  sha256 "d5fc5df47f4718072dabb4e44a5de47c403b3a0c3a1e3cb5c9e52997871ce24f"
+  version "1.7.0"
+  sha256 "b0c93bc551c9299dc2567f676fcd4c7f3fcaaa487605dc585e00a19dce6e50ca"
 
   url "https://starcat.ink/downloads/Starcat-#{version}-arm64.dmg"
   name "Starcat"
